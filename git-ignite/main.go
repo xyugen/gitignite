993a2a718bf02b27358ee0f15cd19fc43d23b418
Alex Arias <alex.arias.jorym@gmail.com>
2023-08-14 22:17:56 +0800

    feat(gitignite): create cli command

--- git-ignite/main.go
+++ git-ignite/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	app := &cli.App{
+		Name:  "gitignite",
+		Usage: "generate .gitignore file from a template",
+		Commands: []*cli.Command{
+			{
+				Name: "init",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "lang",
+						Aliases:  []string{"l"},
+						Usage:    "programming language template",
+						Required: true,
+						Action: func(ctx *cli.Context, s string) error {
+							// Lowercase the string so that it matches the template file name
+							s = strings.ToTitle(s)
+							// Trim the string
+							s = strings.Trim(s, " ")
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
